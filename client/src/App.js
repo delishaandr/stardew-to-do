60712f536c32081c38151732ba71d807a491da72
delishaandr <13519133@std.stei.itb.ac.id>
2023-08-07 23:04:03 +0700

    add react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AddCrop from './pages/AddCrop';
+import AddToDo from './pages/AddToDo';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-crop" element={<AddCrop />} />
+          <Route path="/add-todo" element={<AddToDo />} />
+       </Routes>
+    </>
   );
 }
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+// Home.js
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+ const navigate = useNavigate();
+ return (
+    <div className="container">
+       <div className="title">
+          <h1>Order Product CockTails</h1>
+       </div>
+       <button className="btn" onClick={() => navigate('order-summary')}>
+          Place Order
+       </button>
+    </div>
+ );
+};
+
+export default Home;
