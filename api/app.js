9e8a7c4d53befbf27cf44de8a75560f998de1606
delishaandr <13519133@std.stei.itb.ac.id>
2023-09-21 22:43:11 +0700

    connect to database

--- api/app.js
+++ api/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
+var models = require('./models');
 
 var app = express();
 
