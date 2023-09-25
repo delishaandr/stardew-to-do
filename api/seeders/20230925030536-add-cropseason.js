51195e0200ac0ba625441cf0fcb4adca30617097
delishaandr <13519133@std.stei.itb.ac.id>
2023-09-25 10:33:46 +0700

    seed cropseason

---
+++ api/seeders/20230925030536-add-cropseason.js
@@ -0,0 +1,367 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('CropSeasons', [
+    {
+      cropId: 1,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 2,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 3,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 3,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 4,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 5,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 6,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 7,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 8,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 9,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 10,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 11,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 12,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 13,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 14,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 14,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 15,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 16,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 17,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 18,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 19,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 20,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 21,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 22,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 22,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 23,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 24,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 24,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 25,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 24,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 25,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 26,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 27,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 28,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 29,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 30,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 31,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 32,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 33,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 34,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 35,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 35,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 35,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 36,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 36,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 36,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 36,
+      seasonId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 37,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 37,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 37,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 37,
+      seasonId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 38,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 39,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 40,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 41,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 42,
+      seasonId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 42,
+      seasonId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 42,
+      seasonId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      cropId: 42,
+      seasonId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('CropSeasons', null, {});
+  }
+};
